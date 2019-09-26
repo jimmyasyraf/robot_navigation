7f4faf88b53d29673a7e6fabf813145cdcfeeb00
Hazimi Asyraf <jimmyasyraf@gmail.com>
2019-09-26 00:00:52 -0400

    graceful exit

--- src/obstacle_avoidance.py
+++ src/obstacle_avoidance.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 import rospy
-from ros_tutorial.msg import Distance
-from ros_tutorial.msg import Direction
+from robot_navigation.msg import Distance
+from robot_navigation.msg import Direction
 
 straight = 1
 right = 2
 left = 3
+stop = 4
 
 pub = rospy.Publisher('direction', Direction, queue_size=10)
 
@@ -15,27 +16,32 @@ def decide_direction(distance):
     distance3 = distance.distance3
     direction = straight
 
-    if (distance1 > distance2) and (distance1 > distance3):
+    if (distance1 < 20):
+        if (distance2 > distance3):
+            direction = right
+        else:
+            direction = left
+    else:
         direction = straight
-    elif (distance2 > distance1) and (distance2 > distance3):
-        direction = right
-    elif (distance3 > distance1) and (distance3 > distance2):
-        direction = left
 
     return direction
 
 def callback(data):
     direction = decide_direction(data)
-    #rospy.loginfo("Front: {}, Right: {}, Left: {}".format(data.distance1, data.distance2, data.distance3))
     rospy.loginfo(direction)
     msg = Direction()
     msg.direction = direction
     pub.publish(msg)
 
+def exit_gracefully():
+    msg = Direction()
+    msg.direction = stop
+    pub.publish(msg)
+
 def obstacle_avoidance():
     rospy.init_node('obstacle_avoidance', anonymous=True)
     rospy.Subscriber("distance", Distance, callback)
-
+    rospy.on_shutdown(exit_gracefully)
     rospy.spin()
 
 if __name__ == '__main__':
